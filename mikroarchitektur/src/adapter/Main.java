c39190888215cf811763aa4b27d997d4c8a36f98
beatlesally <sevoetter@tsn.at>
2022-10-28 11:01:20 +0200

    minor changes

--- mikroarchitektur/src/adapter/Main.java
+++ mikroarchitektur/src/adapter/Main.java
@@ -1,5 +1,6 @@
 package adapter;
 
+//Example from https://www.baeldung.com/java-adapter-pattern
 public class Main {
     public static void main(String[] args) {
         Movable car1 = new LuxuryCar();
