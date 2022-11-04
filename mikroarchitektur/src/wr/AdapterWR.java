3cfb059da670406eedcbc91ee169d4013042c535
beatlesally <sevoetter@tsn.at>
2022-11-04 16:35:17 +0100

    commented AdapterWR und ISammelumrechnung

--- mikroarchitektur/src/wr/AdapterWR.java
+++ mikroarchitektur/src/wr/AdapterWR.java
@@ -1,13 +1,28 @@
 package wr;
 
+/**
+ * Diese Klasse ist der Adapter, der die Verwendung von einer Sammelumrechnung
+ * in Verbindung von IUmrechnen ermöglicht.
+ */
 public class AdapterWR implements ISammelumrechnung{
 
     private WR umrechner;
 
+    /**
+     * Im Konstruktor wird der Umrechner festgelegt.
+     * @param umrechner ein Objekt für die Umrechnung
+     */
     public AdapterWR(WR umrechner){
         this.umrechner = umrechner;
     }
 
+    /**
+     * In dieser Methode erfolgt die Sammelumrechnung. Hier wird für jeden Betrag
+     * eine Umrechnung einzeln durchgeführt und das Ergebnis zum Gesamtbetrag dazugezählt.
+     * @param betraege Array mit Beiträgen die umgerechnet werden sollen
+     * @param variante die Umrechnungsvariante für die Beträge
+     * @return den umgerechneten Gesamtbetrag
+     */
     @Override
     public double sammelumrechnen(double[] betraege, String variante) {
         double gesamtbetrag = 0;
--- mikroarchitektur/src/wr/ISammelumrechnung.java
+++ mikroarchitektur/src/wr/ISammelumrechnung.java
@@ -1,5 +1,14 @@
 package wr;
 
+/**
+ * Dieses Interface ist für Sammelumrechnungen zuständig.
+ */
 public interface ISammelumrechnung {
+    /**
+     * Die Methode ist für die Sammelumrechnung zuständig.
+     * @param betraege Array mit Beiträgen die umgerechnet werden sollen
+     * @param variante die Umrechnungsvariante für die Beträge
+     * @return die umgerechnete Gesamtsumme
+     */
     public double sammelumrechnen(double[] betraege, String variante);
 }
