83ce77a6be13bcc785325840f7cd85d2c4985151
beatlesally <sevoetter@tsn.at>
2022-11-04 18:10:27 +0100

    finished log file observer

--- wr_observer/src/main/java/wr/LogObserver.java
+++ wr_observer/src/main/java/wr/LogObserver.java
@@ -2,17 +2,24 @@ package wr;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 public class LogObserver implements Observer {
     @Override
     public void update(Observable o, Object arg) {
+
+        String[] list = (String[]) arg;
+        String[] cutWR = list[0].split("2");
+
         try {
-            FileWriter myWriter = new FileWriter("C:\\school\\5AAIF\\FSE_LAND\\sv_fse_land\\wr_observer\\src\\main\\java\\wr\\logs.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            FileWriter myWriter = new FileWriter("C:\\school\\5AAIF\\FSE_LAND\\sv_fse_land\\wr_observer\\src\\main\\java\\wr\\logs.txt",true);
+            myWriter.write(new Date()+"\t");
+            myWriter.write("Ausgangswährung: "+ cutWR[0] + "\t");
+            myWriter.write("Ausgangsbetrag: "+ list[1] + "\t");
+            myWriter.write("Zielwährung: "+ cutWR[1] + "\t");
+            myWriter.write("Zielbetrag: "+ list[2] + "\t\n");
             myWriter.close();
-            System.out.println("Successfully wrote to the file.");
+            System.out.println("Successfully wrote to the log file.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
