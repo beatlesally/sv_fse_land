8c631a24ce593b0575c5aabc5623aa6fd3345533
beatlesally <sevoetter@tsn.at>
2022-11-23 10:06:31 +0100

    insert statement

--- datenbankzugriff/jdbcdemo/src/main/java/it/kolleg/JDBCDemo.java
+++ datenbankzugriff/jdbcdemo/src/main/java/it/kolleg/JDBCDemo.java
@@ -6,9 +6,43 @@ public class JDBCDemo {
 
     public static void main(String[] args) {
         System.out.println( "JDBC Demo" );
-        //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Michi Vötter', 'mvoetter@tsn.at');
+
 
         selectAllDemo();
+        insertStudentDemo();
+        selectAllDemo();
+    }
+
+    public static void insertStudentDemo(){
+        System.out.println("insert Demo with JDBC");
+        String insertStudent = "SELECT * FROM `student`"; //SELECT Statement
+
+        String connectionUrl = "jdbc:mysql://localhost:3306/jdbcdemo"; //Über welche URL + DB soll zugegriffen werden
+        String user = "root";
+        String pwd = "";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd))
+        {
+            System.out.println("Verbindung zur DB erfolgreich");
+
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, ?, ?)"); //SQL Statement vorbereiten
+
+            try
+            {
+                preparedStatement.setString(1, "Peter Zeck"); //Datenwerte eintragen bei ?
+                preparedStatement.setString(2, "p.zeck@gmail.com");
+                int rowAffected= preparedStatement.executeUpdate();
+                System.out.println("Rows affected: "+rowAffected);
+            } catch (SQLException e)
+            {
+                System.out.println("Fehler beim SQL-Statement: "+e.getMessage());
+            }
+
+        } catch (SQLException e)
+        {
+            System.out.println("Fehler beim Aufbau zur Verbindung zur DB: "+ e.getMessage());
+        }
     }
 
     public static void selectAllDemo(){
