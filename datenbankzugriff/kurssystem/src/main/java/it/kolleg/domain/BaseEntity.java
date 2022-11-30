3b1bd8ca00e31f007f3545d481a173f997c358f3
beatlesally <sevoetter@tsn.at>
2022-11-30 09:57:44 +0100

    business logic in Course.java

--- datenbankzugriff/kurssystem/src/main/java/it/kolleg/domain/BaseEntity.java
+++ datenbankzugriff/kurssystem/src/main/java/it/kolleg/domain/BaseEntity.java
@@ -1,20 +1,34 @@
 package it.kolleg.domain;
 
+/**
+ * Basisklasse für alle Entities, die wird später definieren möchten.
+ */
 public abstract class BaseEntity {
-    public Long id;
+    private Long id;
 
     public BaseEntity(Long id)
     {
+        setId(id);
+    }
 
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id)
     {
-        if(id==null || id >= 0) //wenn id null -> insert! A_I || wenn id >= 0 Eintrag in DB vorhanden -> update!
+        if(id==null || id >= 0) //wenn id null -> insert! weil A_I || wenn id >= 0 Eintrag in DB vorhanden -> update!
         {
             this.id = id;
         } else {
             throw new InvalidValueException("Kurs-ID muss größer gleich 0 sein!");
         }
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
     }
 }
--- datenbankzugriff/kurssystem/src/main/java/it/kolleg/domain/Course.java
+++ datenbankzugriff/kurssystem/src/main/java/it/kolleg/domain/Course.java
@@ -2,7 +2,7 @@ package it.kolleg.domain;
 
 import java.sql.Date;
 
-public class Course {
+public class Course extends BaseEntity {
 
     private String name;
     private String descr;
@@ -10,4 +10,110 @@ public class Course {
     private Date beginDate;
     private Date endDate;
     private CourseType courseType;
+
+    public Course(Long id) {
+        super(id);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws InvalidValueException { //muss nicht, weil unhandled expection weil von RuntimeException erbt
+        if (name != null && name.length() > 1) {
+            this.name = name;
+        } else {
+            throw new InvalidValueException("Kursname muss mindestens 2 Zeichen lang sein!");
+        }
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) throws InvalidValueException {
+        if (descr != null && descr.length() > 10) {
+            this.descr = descr;
+        } else {
+            throw new InvalidValueException("Kursbezeichnung muss mindestens 10 Zeichen lang sein!");
+        }
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) throws InvalidValueException {
+        if (hours > 0 && hours < 10) {
+            this.hours = hours;
+        } else {
+            throw new InvalidValueException("Kursstunden muss zwischen 1 und 9 liegen!");
+        }
+        this.hours = hours;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) throws InvalidValueException {
+        if (beginDate != null) {
+            if (this.endDate != null) {
+                if (beginDate.before(this.endDate)) {
+                    this.beginDate = beginDate;
+                } else {
+                    throw new InvalidValueException("Kursbeginn muss vor Kursende sein!");
+                }
+            } else {
+                this.beginDate = beginDate;
+            }
+        } else {
+            throw new InvalidValueException("Startdatum darf nicht leer sein!");
+        }
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) throws InvalidValueException {
+        if (endDate != null) {
+            if (this.beginDate != null) {
+                if (endDate.after(this.beginDate)) {
+                    this.endDate = endDate;
+                } else {
+                    throw new InvalidValueException("Kursende muss nach Kursbeginn sein!");
+                }
+            } else {
+                this.endDate = endDate;
+            }
+        } else {
+            throw new InvalidValueException("Enddatum darf nicht leer sein!");
+        }
+    }
+
+    public CourseType getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(CourseType courseType) throws InvalidValueException {
+        if (courseType != null) {
+            this.courseType = courseType;
+        } else {
+            throw new InvalidValueException("Kurstyp darf nicht leer sein!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + this.getId() +
+                ", name='" + name + '\'' +
+                ", descr='" + descr + '\'' +
+                ", hours=" + hours +
+                ", beginDate=" + beginDate +
+                ", endDate=" + endDate +
+                ", courseType=" + courseType +
+                '}';
+    }
 }
