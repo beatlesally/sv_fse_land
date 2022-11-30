6a5f61c896975e5baea6391aa7837fdc42246897
beatlesally <sevoetter@tsn.at>
2022-11-30 08:47:27 +0100

    started with jdbc intro 2 (kurssystem)

---
+++ datenbankzugriff/kurssystem/src/main/java/it/kolleg/App.java
@@ -0,0 +1,13 @@
+package it.kolleg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
