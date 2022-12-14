0e34d0387d025706813b82efcdd83a2126953c6c
beatlesally <sevoetter@tsn.at>
2022-12-14 08:44:15 +0100

    started live demo jokeapp

---
+++ springboot/jokeapp-springbootdemo/src/main/java/at/itkolleg/jokeappspringbootdemo/JokeappSpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package at.itkolleg.jokeappspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappSpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappSpringbootdemoApplication.class, args);
+    }
+
+}
---
+++ springboot/jokeapp-springbootdemo/src/test/java/at/itkolleg/jokeappspringbootdemo/JokeappSpringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.itkolleg.jokeappspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappSpringbootdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
